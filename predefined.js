29c7922e937bc89712ae656dc6286f48f4f660b7
nandini <nandini232002@gmail.com>
2025-09-22 15:58:37 +0530

    update predefined.js

---
+++ predefined.js
@@ -0,0 +1,7 @@
+// Predefined objects in JavaScript
+let d = new Date();              // Date
+let r = /abc/;                   // RegExp
+let m = new Map();               // Map
+let s = new Set([1, 2, 3]);      // Set
+
+console.log(d, r, m, s);
