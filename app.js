eeaf1452253a0d9b5f73cd79bce23bfc46d2d0e7
nandini <nandini232002@gmail.com>
2025-09-22 14:01:27 +0530

    update index.html

---
+++ app.js
@@ -0,0 +1,13 @@
+
+
+var name = "Nandini";   // String
+var age = 20;           // Number
+var isStudent = true;   // Boolean
+var city;               // Undefined
+var car = null;         // Null
+var fruits = ["Apple", "Mango"]; // Array
+var person = {fname: "Nandini", age: 21}; // Object
+
+console.log(name, age, isStudent, city, car, fruits, person);
+
+
---
+++ function.js
@@ -0,0 +1,8 @@
+// Define a function named 'addNumbers'
+function addNumbers(a, b) {
+    return a + b; // Return the sum of a and b
+}
+
+// Call the function
+let sum = addNumbers(5, 10);
+console.log("Sum:", sum); 
